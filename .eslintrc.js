c5519b7633c0a2d63d9cd217e96fb89de430dc3c
Seokjun Hong <sjhong@ocube.co.kr>
2023-08-02 09:41:33 +0900

    [feat]  - for JH

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+    'react/no-unknown-property': [
+      1,
+      {
+        ignore: ['tsx'],
+      },
+    ],
+  },
+}
